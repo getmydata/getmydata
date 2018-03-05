26596a1b43cdb7589f9cd6e04a456af1c46b19ea
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-05 14:07:36 +0100

    Heroku

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
