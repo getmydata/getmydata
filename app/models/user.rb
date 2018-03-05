0194b2016c13fa81ce4ba26d59c7ff94570b5492
Steef <stefanodegenkamp@gmail.com>
2018-03-05 18:48:13 +0100

    User model made

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :messages
+  has_many :reviews
+  has_many :companies, through: :messages
+
+  # has_many :companies_reviewed, through: :reviews, foreign_key: "company_id", class_name: "Company"
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
