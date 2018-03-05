e7d4cbf01903a68b9df4b90f567d39e9b25dd3cd
Rick te Molder <rtminnovations@gmail.com>
2018-03-05 17:18:11 +0100

    Update company.rb

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ApplicationRecord
   has_many :reviews
   has_many :messages
   has_many :users, through: :messages
+  has_many :users, through: :reviews
 
   validates :name, uniqueness: true
 end
