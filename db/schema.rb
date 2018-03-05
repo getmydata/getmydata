cc52e16dc5cef361945c2f84ca29a2f3ac43344f
Rick te Molder <rtminnovations@gmail.com>
2018-03-05 18:00:27 +0100

    added category to companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305154005) do
+ActiveRecord::Schema.define(version: 20180305164558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180305154005) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "messages", force: :cascade do |t|
