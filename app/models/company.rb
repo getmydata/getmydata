377c2f183abdb03725088d77d22fe2c84890eacf
Steef <stefanodegenkamp@gmail.com>
2018-03-06 11:49:51 +0100

    Has_many/belongs_to and validations added to each model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,12 @@
 class Company < ApplicationRecord
-  has_many :reviews
   has_many :messages
+  has_many :reviews
   has_many :users, through: :messages
-  has_many :users, through: :reviews
+  # has_many :users, through: :reviews > isn't possible to have both
 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :url, presence: true
   validates :name, uniqueness: true
+  # validates :category, inclusion: { in: ["living", "energy", "news", "bank", "education"] } > still have to decide whichs categories
 end
