9433e1a5b7ea3ef9b46b384023cecd9793d997da
Steef <stefanodegenkamp@gmail.com>
2018-03-06 14:24:48 +0100

    Commented recources :users because we have device

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users
+  # resources :users
   resources :companies
   get '/profile', to: "users#profile", as: :profile
   get '/show', to: "users#show", as: :show
