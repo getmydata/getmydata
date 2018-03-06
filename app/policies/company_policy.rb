b9a8c441775188195484d3c1076f8ea9fdc6b1b9
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-06 15:37:18 +0100

    Form style quick fix with flexbox

---
+++ app/policies/company_policy.rb
@@ -0,0 +1,28 @@
+class CompanyPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+end
