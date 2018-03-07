6be3690d539e93288ff579d5110cb63bf942e45c
Stefano Degenkamp <stefanodegenkamp@gmail.com>
2018-03-07 17:47:46 +0100

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_message, only: [:show, :update, :destroy]
   # before_action :generate_token, only: [:create]
 
   def index
@@ -41,10 +41,6 @@ class MessagesController < ApplicationController
       render :new
     end
     # raise
-  end
-
-  def edit
-    # only authorization for admin
   end
 
   def destroy
