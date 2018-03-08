9b4cdd1b9d313b4579209be283971e5bf8171421
Rick te Molder <rtminnovations@gmail.com>
2018-03-08 13:24:56 +0100

    avatar shows now after oploading it

--- app/uploaders/identification_uploader.rb
+++ app/uploaders/identification_uploader.rb
@@ -1,3 +1,13 @@
 class IdentificationUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+    storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(pdf)
+  end
 end
