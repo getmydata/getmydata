e9cd021cf11ba132583d01b7735ea0205fa0d646
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-11 22:52:27 +0100

    Small layout and indentation changes

--- app/pdfs/profile_pdf.rb
+++ app/pdfs/profile_pdf.rb
@@ -24,8 +24,10 @@ class ProfilePdf < Prawn::Document
     text "Password:", size: 14, style: :bold
     text "#{@user.encrypted_password}", size: 14, style: :normal
     move_down 10
+    text "Avatar:", size: 14, style: :bold
     image open("#{@user.avatar}"), :width => 100
     move_down 10
+    text "identification:", size: 14, style: :bold
     image open("#{@user.identification}"), :width => 300
     move_down 10
     text "Sign in count:", size: 14, style: :bold
