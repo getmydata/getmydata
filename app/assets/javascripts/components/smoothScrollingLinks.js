0d03bced3fa3ba4125445c05908efba75889f1d7
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-11 14:53:27 +0100

    Homepage setup with smooth scrolling bullets

--- app/assets/javascripts/components/smoothScrollingLinks.js
+++ app/assets/javascripts/components/smoothScrollingLinks.js
@@ -5,7 +5,7 @@
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 1000, 'swing' ,function(){
         window.location.hash = hash;
       });
     }
