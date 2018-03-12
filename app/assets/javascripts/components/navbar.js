55c2993ad520bd7f8fc89003d3234eaab696c0c6
Stefano Degenkamp <stefanodegenkamp@gmail.com>
2018-03-12 17:22:28 +0100

    Space

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {     // Transition effect for navbar
   $(window).scroll(function() {     // checks if window is scrolled more than 500px, adds/removes solid class
     if($(this).scrollTop() > 700) {
