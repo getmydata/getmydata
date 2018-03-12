50d17443903614e719a290a6580b0f035c43c6b8
Rick te Molder <rtminnovations@gmail.com>
2018-03-12 19:37:53 +0100

    changed colors from blue to dark grey, banner white

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {     // Transition effect for navbar
   $(window).scroll(function() {     // checks if window is scrolled more than 500px, adds/removes solid class
-    if($(this).scrollTop() > 700) {
+    if($(this).scrollTop() > 750) {
       $('.navbar-wagon-home').addClass('solid');
     }
     else {
