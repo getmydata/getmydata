0c8dbe6741798628cf78e43e4017768f0ac4bf5c
Rick te Molder <rtminnovations@gmail.com>
2018-03-12 17:20:58 +0100

    navbar only transparent on first part of homepage

---
+++ app/assets/javascripts/components/navbar.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {     // Transition effect for navbar
+  $(window).scroll(function() {     // checks if window is scrolled more than 500px, adds/removes solid class
+    if($(this).scrollTop() > 700) {
+      $('.navbar-wagon-home').addClass('solid');
+    }
+    else {
+      $('.navbar-wagon-home').removeClass('solid');
+    }
+  });
+});
