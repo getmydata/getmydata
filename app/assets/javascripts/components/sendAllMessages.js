88179ab49b4985cd54d776af03e13de9c08a7f8c
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-13 16:39:20 +0100

    Added sweet alert

--- app/assets/javascripts/components/sendAllMessages.js
+++ app/assets/javascripts/components/sendAllMessages.js
@@ -1,19 +1,22 @@
 $("#send-all-messages").click(function(){
-  if(confirm("Send all messages?")){
-    const forms = document.getElementsByClassName("total-forms");
+  const forms = document.getElementsByClassName("total-forms");
     for (var i=0 ; i<forms.length ; i++){
       document.getElementById("click-message-form"+i).click();
     }
 
-    event.preventDefault();
-
-    setTimeout(function(){
-      window.alert("Messages have been send");
-      window.location.href = "/users/id/messages";
-    }, 1000);
-
+    event.preventDefault()
+    swal({
+  title: 'Requests send!',
+  html: 'Thank you!',
+  type: 'success',
+  confirmButtonText: 'View my messages!'
+}).then(function () {
+  window.location.href = "/users/id/messages";
+}, function (dismiss) {
+  // dismiss can be 'cancel', 'overlay',
+  // 'close', and 'timer'
+  if (dismiss === 'cancel') {
+    window.location.href = "/send-messages";
   }
-  else{
-    return false;
-  }
+  });
 });
