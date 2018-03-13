e27656013b123366eaf2701790cbc87ff1e03f8c
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-13 09:35:12 +0100

    Remove bullet navigation from homepage

--- app/assets/javascripts/components/homePageBulletNavigation.js
+++ app/assets/javascripts/components/homePageBulletNavigation.js
@@ -12,28 +12,28 @@ $(document).scroll(function() {
     bulletNavigation();
 });
 
-$.fn.isOnScreen = function(){
+// $.fn.isOnScreen = function(){
 
-    var win = $(window);
+//     var win = $(window);
 
-    var viewport = {
-        top : win.scrollTop(),
-        left : win.scrollLeft()
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
+//     var viewport = {
+//         top : win.scrollTop(),
+//         left : win.scrollLeft()
+//     };
+//     viewport.right = viewport.left + win.width();
+//     viewport.bottom = viewport.top + win.height();
 
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
+//     var bounds = this.offset();
+//     bounds.right = bounds.left + this.outerWidth();
+//     bounds.bottom = bounds.top + this.outerHeight();
 
-    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+//     return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
 
-};
+// };
 
-$('body').click(function(){
-    alert($('#anchor-1').isOnScreen());
-});
+// $('body').click(function(){
+//     alert($('#anchor-1').isOnScreen());
+// });
 
 // if($('#anchor-1').isOnScreen()) {
 //   alert("Anchor-1 on screen")
