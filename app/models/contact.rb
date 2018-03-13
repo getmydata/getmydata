b64d1eac779c76ba6bd17a992db8642ae9d582b7
Steef <stefanodegenkamp@gmail.com>
2018-03-13 16:10:21 +0100

    Form build with prefilled name and email if user is logged in

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@ class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,    :validate => true
-  # attribute :nickname,  :captcha  => true
+  attribute :nickname,  :captcha  => true
 
   def headers
     {
