a06cc8225fcc123f9d10e0c490cada3dcad4bbe0
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-14 12:17:24 +0100

    Change forms part 1

---
+++ app/assets/javascripts/components/signUpSignIn.js
@@ -0,0 +1,54 @@
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+  var $this = $(this),
+  label = $this.prev('label');
+
+  if (e.type === 'keyup') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    } else {
+      label.addClass('active highlight');
+    }
+  } else if (e.type === 'blur') {
+    if( $this.val() === '' ) {
+      label.removeClass('active highlight');
+    } else {
+      label.removeClass('highlight');
+    }
+  } else if (e.type === 'focus') {
+
+    if( $this.val() === '' ) {
+      label.removeClass('highlight');
+    }
+    else if( $this.val() !== '' ) {
+      label.addClass('highlight');
+    }
+  }
+
+});
+
+$('.tab a').on('click', function (e) {
+
+  e.preventDefault();
+
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
+});
+
+
+
+if(window.location.href.includes("users/sign_up")) {
+  console.log("Hoi")
+
+  $(function() {
+    console.log( "ready!" );
+    $('#signup-click a').trigger('click');
+  });
+};
