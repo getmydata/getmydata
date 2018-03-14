753462b8d7242b82b17ebbf7c36d39ea346b7cf7
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-14 20:29:34 +0100

    Lower the px for scrolltop so navbar on homepage appears earlier

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {     // Transition effect for navbar
   $(window).scroll(function() {     // checks if window is scrolled more than 500px, adds/removes solid class
-    if($(this).scrollTop() > 750) {
+    if($(this).scrollTop() > 150) {
       $('.navbar-wagon-home').addClass('solid');
       $('.navbar-wagon-home').addClass('solid');
     }
