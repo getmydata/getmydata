1a3f0edc6a545f9c0dfdf2327d5f4c10e6a004dc
Madelene Casselbrant <madelene.casselbrant@gmail.com>
2018-03-22 14:24:49 +0100

    DRY messages controller code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,65 +2,53 @@ class MessagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :destroy]
   skip_before_action :verify_authenticity_token
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :send_messages, :show]
+  before_action :set_messages, only: [:index]
+  before_action :set_company, only: [:edit, :update]
 
   def index
-   @user = current_user
-   @user_selections = current_user.user_selections.all
-   @Messages = Message.all
-   policy_scope(Message)
- end
 
- def send_messages
-   @user = current_user
-   authorize @user
-   @message = Message.new
-   @user_selections = current_user.user_selections.all
-   @Messages = Message.all
-   policy_scope(Message)
- end
+  end
 
- def show
-  @user = current_user
-  @message = Message.find(params[:id])
-  policy_scope(Message)
-end
+  def send_messages
+    authorize @user
+    @message = Message.new
+    set_messages
+  end
 
-def new
-  @message = Message.new
-  @company = Company.find(params[:company_id])
-  authorize @message
-end
+  def show
+    policy_scope(Message)
+  end
 
-def create
-  @message = Message.new(message_params)
-  @message.created_at = Time.now
-  @company = Company.find(params[:company_id])
-
-    # Code below is the standard to set current_user to message.user_id. For the demo I have to work around this. Reset after demo!!!!!!!
-    # @message.user = current_user
-    @message.company = @company
+  def new
+    @message = Message.new
+    set_company
     authorize @message
-    if @message.save
-      respond_to do |format|
-        format.html { redirect_to user_messages_path(current_user) }
-        format.js  # <-- will render `app/views/reviews/create.js.erb`
-      end
-    else
-      respond_to do |format|
-        format.html { render :new }
-        format.js  # <-- will render `app/views/reviews/create.js.erb`
-      end
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.created_at = Time.now
+    set_company
+    @message.user = current_user
+    authorize @message
+    respond_to do |format|
+      format.html {
+        if @message.save
+          redirect_to user_messages_path(current_user)
+        else
+          render :new
+        end
+      }
+      format.js  # <-- will render `app/views/reviews/create.js.erb`
     end
   end
 
   def edit
-    @company = Company.find(params[:company_id])
-    @message.company = @company
+
   end
 
   def update
-    @company = Company.find(params[:company_id])
-    @message.company = Company.find(params[:company_id])
     @message.update(message_params)
     redirect_to user_messages_path(:user_id)
   end
@@ -73,21 +61,32 @@ def create
 
   protected
 
+  # strong params for messages
+  def message_params
+    params.require(:message).permit(:date, :sent_by_user, :subject, :text, :attachment, :request_id, :user_id, :company_id, :start_date, :end_date)
+  end
+
+  # Defines user selections, messages and Message-policy scope
+  def set_messages
+    @user_selections = @user.user_selections.all
+    @messages = Message.all
+    policy_scope(Message)
+  end
+
+  # Defines a specific message
   def set_message
     @message = Message.find(params[:id])
     authorize @message
   end
 
-  def message_params
-    params.require(:message).permit(:date, :sent_by_user, :subject, :text, :attachment, :request_id, :user_id, :company_id, :start_date, :end_date)
+  # Defines user as current user
+  def set_user
+     @user = current_user
   end
 
-  # def generate_token
-  #   self.request_id = loop do
-  #     random_token = SecureRandom.urlsafe_base64(nil, false)
-  #     break random_token unless ModelName.exists?(token: random_token)
-  #   end
-  # end
-
+  # Defines a specific company and
+  def set_company
+    @company = Company.find(params[:company_id])
+    @message.company = @company
+  end
 end
-
