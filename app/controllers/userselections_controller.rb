7bacf5e76a253be1821acaefe0c36c507b43530d
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-22 14:16:08 +0100

    Select companies link login

--- app/controllers/userselections_controller.rb
+++ app/controllers/userselections_controller.rb
@@ -2,11 +2,9 @@ class UserselectionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:select, :create ]
 
   def select
-    if user_signed_in?
-      @user_selection = UserSelection.new
-      @user_selections = UserSelection.all
-      @companies = Company.all
-    end
+    @user_selection = UserSelection.new
+    @user_selections = UserSelection.all
+    @companies = Company.all
 
     policy_scope(Company)
     if params[:query].present?
@@ -43,37 +41,37 @@ class UserselectionsController < ApplicationController
     @selection_array = []
     @user_selections = UserSelection.where(user: current_user)
     if @user_selections.present?
-       @user_selections.each do |selection|
-        @selection_array << selection
-      end
-    end
-    @unselected_companies = []
-    @companies.each do |company|
-      if @selection_array.include?(company)
-        @unselected_companies << company
-      end
-    end
-    authorize @user_selection
-    if @user_selection.save
-      respond_to do |format|
-        format.html { redirect_to select_path }
-        format.js
-      end
-    else
-      respond_to do |format|
-        format.html { render 'userselections/select'}
-        format.js
-      end
+     @user_selections.each do |selection|
+      @selection_array << selection
     end
   end
+  @unselected_companies = []
+  @companies.each do |company|
+    if @selection_array.include?(company)
+      @unselected_companies << company
+    end
+  end
+  authorize @user_selection
+  if @user_selection.save
+    respond_to do |format|
+      format.html { redirect_to select_path }
+      format.js
+    end
+  else
+    respond_to do |format|
+      format.html { render 'userselections/select'}
+      format.js
+    end
+  end
+end
 
-  def destroy
+def destroy
     # authorize current_user
     @user_selection = UserSelection.find(params[:id])
     @user_selection.destroy
     authorize @user_selection
     policy_scope(UserSelection)
-    # redirect_to request.referrer
+
 
   end
 
@@ -81,7 +79,7 @@ class UserselectionsController < ApplicationController
 
   def user_selection_params
    params.require(:user_selection).permit(:company_id)
-  end
+ end
 end
 
  # @companies.each do |company|
