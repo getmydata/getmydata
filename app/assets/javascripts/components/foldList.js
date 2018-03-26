2011b3a2372606206c6cb49b4cdb2da0f475f7aa
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-26 17:37:49 +0200

    Expand list

--- app/assets/javascripts/components/foldList.js
+++ app/assets/javascripts/components/foldList.js
@@ -2,6 +2,7 @@
 
 var headers = ["Living", "Energy", "News", "Bank", "Education"]
 
+// !!! Copy the headers array from company.rb model !!!
 
 var arrayLength = headers.length
 
@@ -9,14 +10,25 @@ for (var i = 0; i < arrayLength; i++) {
   header = headers[i].toLowerCase();
 
   $(`.header-${header}`).click(function () {
+
     $header = $(this);
+
     //getting the next element
     $content = $header.next();
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $content.slideToggle(500, function () {
-        //execute this after slideToggle is done
-        //change text of header based on visibility of content div
-      });
+
+    for (var i = 0; i < arrayLength; i++) {
+      headerNotSelect = headers[i].toLowerCase();
+      if (($(`.header-${headerNotSelect}`)[0]) != (($header)[0])) {
+        $(`.header-${headerNotSelect}`).next().slideUp(500, function () {});
+      }
+    }
+
+    if (($content).is(':visible')) {
+      $content.slideUp(500);
+    } else {
+      $content.slideDown(500);
+    }
 
   });
 }
