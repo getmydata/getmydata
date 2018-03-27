60862738fac4f679b241c892d0a1197c8e9d7c77
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-27 13:13:06 +0200

    Avatar default

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,7 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+   def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "user.png"].compact.join('_'))
+  end
 end
