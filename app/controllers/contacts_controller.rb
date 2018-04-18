46fae34c82f509195d5229e39e416bca835c80ed
Pim Zonneveld <pimzonneveld@gmail.com>
2018-04-18 09:56:43 +0200

    Add send mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,10 @@
 class ContactsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
 
+  require 'sendgrid-ruby'
+  include SendGrid
+  require 'json'
+
   def new
     @contact = Contact.new
     authorize @contact
@@ -17,5 +21,19 @@ class ContactsController < ApplicationController
     #   flash[:error] = 'Cannot send message.'
     #   render :new
     # end
+  end
+
+  def hello_world
+    from = Email.new(email: 'test@example.com')
+    to = Email.new(email: 'work@pim.gg')
+    subject = 'Contact Form'
+    content = Content.new(type: 'text/plain', value: 'Hoi Rick. Groetjes vanaf de GetMyData website')
+    mail = Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 end
