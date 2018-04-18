ae67e55afab2b40388a6629edc80cbba8af14fa9
Pim Zonneveld <pimzonneveld@gmail.com>
2018-04-18 10:21:47 +0200

    API fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,20 @@ class ContactsController < ApplicationController
   include SendGrid
   require 'json'
 
+  def hello_world
+    from = Email.new(email: 'test@example.com')
+    to = Email.new(email: 'work@pim.gg')
+    subject = 'Contact Form'
+    content = Content.new(type: 'text/plain', value: 'Hoi Rick. Groetjes vanaf de GetMyData website')
+    mail = Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
+  end
+
   def new
     @contact = Contact.new
     authorize @contact
@@ -22,19 +36,5 @@ class ContactsController < ApplicationController
     #   render :new
     # end
     hello_world
-  end
-
-  def hello_world
-    from = Email.new(email: 'test@example.com')
-    to = Email.new(email: 'work@pim.gg')
-    subject = 'Contact Form'
-    content = Content.new(type: 'text/plain', value: 'Hoi Rick. Groetjes vanaf de GetMyData website')
-    mail = Mail.new(from, subject, to, content)
-
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
   end
 end
