d73f2fc267d63faae114c9d6c4ac1ff4ad15edd4
Pim Zonneveld <pimzonneveld@gmail.com>
2018-04-21 14:33:30 +0200

    Email processor change

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,5 +1,17 @@
 class EmailProcessor
-  def self.process(email)
-    Post.create!({ body: email.body, email: email.from })
+  def initialize(email)
+    @email = email
+  end
+
+  def process
+    # all of your application-specific code here - creating models,
+    # processing reports, etc
+
+    # Here's an example of model creation
+    user = User.find_by_email(@email.from[:email])
+    user.posts.create!(
+      subject: @email.subject,
+      body: @email.body
+    )
   end
 end
