faa96871f6eef0fda3f120d424d7fef0d58fccba
Pim Zonneveld <pimzonneveld@gmail.com>
2018-05-02 10:52:54 +0200

    Refactor changes

--- app/controllers/userselections_controller.rb
+++ app/controllers/userselections_controller.rb
@@ -48,20 +48,13 @@ class UserselectionsController < ApplicationController
     @companies = Company.all
     @user_selection = UserSelection.new(user_selection_params)
     user_signed_in? ? @user_selection.user_id = current_user.id : @user_selection.user_id = session[:guest_user_id]
+
     @selection_array = []
     user_signed_in? ? user_selections = UserSelection.where(user_id: current_user.id) : user_selections = UserSelection.where(user_id: session[:guest_user_id])
-    if @user_selections.present?
-      @user_selections.each do |selection|
-        @selection_array << selection
-      end
-    end
 
-    # Create a list of unselected companies and enable AJAX
-    @unselected_companies = []
-    @companies.each do |company|
-      if @selection_array.include?(company)
-        @unselected_companies << company
-      end
+    if @user_selections.present?
+      @selected_companies = @user_selections.map(&:company)
+      @unselected_companies = @companies - @selected_companies
     end
 
     authorize @user_selection
