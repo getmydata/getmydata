8a2f278f3900f34dd90890687135cf3be8ad376e
Pim Zonneveld <pimzonneveld@gmail.com>
2018-05-09 15:13:14 +0200

    Jsify message deadline

--- app/assets/javascripts/components/countDownTimerMessages.js
+++ app/assets/javascripts/components/countDownTimerMessages.js
@@ -8,14 +8,15 @@ var x = setInterval(function() { // Update the count down every 1 second
 
   var now = new Date().getTime(); // Get todays date and time
   var distance = countDownDate - now; // Find the distance between now an the count down date
+  // console.log(distance);
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24)); // Time calculations for days, hours, minutes and seconds
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  val.innerHTML = days + "d " + hours + "h " // Display the result in the element with id="demo"
-  + minutes + "m " + seconds + "s ";
+  val.innerHTML = days + "d " + (hours+13) + "h " // Display the result in the element with id="demo"
+  + (minutes+12) + "m " + seconds + "s ";
   if (distance <= 0) { // If the count down is finished, write some text
     val.innerHTML = "EXPIRED";
   }
