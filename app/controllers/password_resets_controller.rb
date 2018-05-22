9791f295f4150b7b25697511e7d1264258618ce4
Pim Zonneveld <pimzonneveld@gmail.com>
2018-05-22 19:08:06 +0200

    Password reset fix

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,12 +14,14 @@ class PasswordResetsController < ApplicationController
 
   def edit
     skip_authorization
-    @user = User.find_by_password_reset_token!(params[:id])
+    # @user = User.find_by_password_reset_token!(params[:id])
+    @user = User.find_by_password_reset_token!(params[:password_reset_token])
   end
 
   def update
     skip_authorization
-    @user = User.find_by_password_reset_token!(params[:id])
+    # @user = User.find_by_password_reset_token!(params[:id])
+    @user = User.find_by_password_reset_token!(params[:password_reset_token])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
     elsif @user.update_attributes(user_params)
